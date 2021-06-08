67e3ed257a6ffc8f82eed8bdaabe0e6ed7954477
liluacrobat <lilu.acrobat@gmail.com>
2021-06-08 11:08:04 -0400

    Update job_template.sh

--- Kneaddata/Tools/job_template.sh
+++ Kneaddata/Tools/job_template.sh
@@ -13,16 +13,7 @@ echo '--------------------'
 echo 'Summarizing sequencing qualities ...'
 START=`date +%s`
 
-kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref --output kneaddata_output --store-temp-output -t 12
-
-eval "$(/util/common/python/py38/anaconda-2020.07/bin/conda shell.bash hook)"
-conda activate /projects/academic/pidiazmo/projectsoftwares/metaphlan3
-unset PYTHONPATH
-
-mkdir genome_output
-mkdir bowtie2_output
-
-metaphlan kneaddata_output/__SAMPLE_ID___R1_kneaddata_paired_1.fastq,kneaddata_output/__SAMPLE_ID___R1_kneaddata_paired_2.fastq --bowtie2out bowtie2_output/__SAMPLE_ID___metagenome.bowtie2.bz2 --input_type fastq -o genome_output/__SAMPLE_ID___profiled_metagenome.txt --nproc 12
+kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/mice_ref --output kneaddata_output --store-temp-output -t 12
 
 END=`date +%s`
 ELAPSED=$(( $END - $START ))
