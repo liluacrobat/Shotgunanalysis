88a40fec3ec63638fd4e17dbe447916c40c0390f
liluacrobat <lilu.acrobat@gmail.com>
2021-06-08 11:16:10 -0400

    Update job_template.sh

--- Kneaddata/Tools/job_template.sh
+++ Kneaddata/Tools/job_template.sh
@@ -3,20 +3,17 @@
 #SBATCH --time=71:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --job-name="Mothur-__SAMPLE_ID__"
-#SBATCH --output=Mothur-__SAMPLE_ID__.log
+#SBATCH --job-name="KD-__SAMPLE_ID__"
+#SBATCH --output=KD-__SAMPLE_ID__.log
 
 eval "$(/util/common/python/py38/anaconda-2020.07/bin/conda shell.bash hook)"
-export PATH=$PATH:/user/lli59/.local/bin
+conda activate /projects/academic/pidiazmo/projectsoftwares/kneaddata/kneaddata
 
 echo '--------------------'
-echo 'Summarizing sequencing qualities ...'
-START=`date +%s`
+echo 'Filtering ...'
 
-kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/mice_ref --output kneaddata_output --store-temp-output -t 12
+# If samples are from human, we can use only the hg37 database for host removal
+kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/mice_ref --output kneaddata_output -t 12
 
-END=`date +%s`
-ELAPSED=$(( $END - $START ))
-echo 'Summarizing sequencing qualities takes $ELAPSED s'
-
-
+echo 'Succeed'
+echo '--------------------'
