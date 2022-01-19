efaf8bd89df5dce46153fb058cb4dec11430a2ce
liluacrobat <lilu.acrobat@gmail.com>
2022-01-19 18:37:02 -0500

    Update job_template.sh

--- Kneaddata/Tools/job_template.sh
+++ Kneaddata/Tools/job_template.sh
@@ -9,11 +9,12 @@
 eval "$(/util/common/python/py38/anaconda-2020.07/bin/conda shell.bash hook)"
 conda activate /projects/academic/pidiazmo/projectsoftwares/kneaddata/kneaddata
 
+
 echo '--------------------'
 echo 'Filtering ...'
 
 # If samples are from human, we can use only the hg37 database for host removal
-kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/mice_ref --output kneaddata_output -t 12
+kneaddata --input ../__SAMPLE_ID___R1.fastq --input ../__SAMPLE_ID___R2.fastq -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/hg37_ref -db /projects/academic/pidiazmo/projectsoftwares/kneaddata/mice_ref --output kneaddata_output -t 12 --trimmomatic /projects/academic/pidiazmo/projectsoftwares/kneaddata/kneaddata/bin/trimmomatic
 
 echo 'Succeed'
 echo '--------------------'
