020500a570569d4ae65a9b325b22077001d15cf3
liluacrobat <lilu.acrobat@gmail.com>
2023-05-05 11:21:20 -0400

    Add files via upload

--- v0.2/Pipeline_process.sh
+++ v0.2/Pipeline_process.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-DM = /panfs/panfs.cbls.ccr.buffalo.edu/scratch/grp-pidiazmo/Dam
-mkdir $DM/fastq
-cp /panfs/panfs.cbls.ccr.buffalo.edu/scratch/grp-pidiazmo/Dam/*.gz $DM/fastq
+tmux
 cd fastq
-for x in ls *.gz;do gunzip $x;done
+for x in $(ls *.gz);do gunzip $x;done
+
 
 
