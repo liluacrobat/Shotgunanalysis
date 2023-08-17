8feb6cfa20df231ee71b4890e2b22206d0d86324
liluacrobat <lilu.acrobat@gmail.com>
2023-08-17 09:58:05 -0400

    Delete Shotgunanalysis-main-draft directory

--- Shotgunanalysis-main-draft/Pipeline_process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-DM = /panfs/panfs.cbls.ccr.buffalo.edu/scratch/grp-pidiazmo/lu_renew/Dam
-cp /projects/academic/pidiazmo/lu/tmp/*.gz $DM/fastq
-cd fastq
-for x in ls *.gz;do gunzip $x;done
-
-
